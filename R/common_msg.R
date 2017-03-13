2b30ba74a0ba7e284ca3111d71903a7659b093ff
maurolepore <maurolepore@gmail.com>
2017-03-13 09:26:53 -0500

    Add handy messages

---
+++ R/common_msg.R
@@ -0,0 +1,18 @@
+#' Print messages that commonly open documents.
+#'
+#' @return Messages
+#' @export
+#'
+#' @examples
+#' pring_zen
+print_zen <- function() {
+  message("
+    [Design zen](https://github.com/forestgeo/learn/issues/8)
+
+    Contact [Mauro Lepore](maurolepore@gmail.com)(maintainer)
+
+    [Code Philosophy](https://gist.github.com/maurolepore/d21564d746f3c3b608c781fb3baf72b7)
+
+    [Code glossary](https://gist.github.com/maurolepore/f7489d127f4749d76150f373dbbc4843)
+    ")
+  }
