5324068a0d7fd68e74fe1f0e2167550bb22e2109
maurolepore <maurolepore@gmail.com>
2017-07-13 11:23:54 -0500

    Add function

---
+++ R/tag.R
@@ -0,0 +1,33 @@
+#' Tag a line in a file with and optional message.
+#'
+#' Tags help track information because they can be searched in RStudio with the
+#' shortcut `Ctrl+.` ("Go To File/Function...").
+#'
+#' @param msg An optional message (default is an empty message).
+#'
+#' @return A closure (see [Closures](https://goo.gl/mHvapg) in [Advanced
+#'   R](http://adv-r.had.co.nz/) by Hadley Whickham).
+#' @export
+#'
+#' @examples
+#'
+#' # Tag a line in a file, so you can come back to that line later easily.
+#' fig_key <- tag("Figure showhing key results.")
+#' # <cool code>
+#' To locate the tag again, search it in RStudio with the shortcut Ctrl+.
+#'
+#' # Tags are handy, for example, to recover your last job.
+#' xxxnext <- tag("Dear future me, continue working here.")
+#' xxxnext()
+#'
+#' # Giving a message is optional.
+#' xxxnext <- tag()
+#' xxxnext()
+#'
+#' # You may track tags in you manuscript, to link text and code.
+tag <- function(msg = "") {
+  function() {
+    message(msg)
+    invisible(msg)
+  }
+}
