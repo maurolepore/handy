50b01d7328d8a914defc39a412117f48c527d97b
maurolepore <maurolepore@gmail.com>
2017-09-04 07:55:47 -0400

    Rewrite tag to print a tag label.

--- R/tag.R
+++ R/tag.R
@@ -1,33 +1,18 @@
-#' Tag a line in a file with and optional message.
-#'
-#' Tags help track information because they can be searched in RStudio with the
-#' shortcut `Ctrl+.` ("Go To File/Function...").
-#'
-#' @param msg An optional message (default is an empty message).
-#'
-#' @return A closure (see [Closures](https://goo.gl/mHvapg) in [Advanced
-#'   R](http://adv-r.had.co.nz/) by Hadley Whickham).
-#' @export
-#'
-#' @examples
-#'
-#' # Tag a line in a file, so you can come back to that line later easily.
-#' fig_key <- tag("Figure showhing key results.")
-#' # <cool code>
-#' # To locate the tag again, search it in RStudio with the shortcut Ctrl+.
-#'
-#' # You may tag the next item in your "to do" list.
-#' todo <- tag("Dear future me, continue working here.")
-#' todo()
-#'
-#' # Giving a message is optional.
-#' todo <- tag()
-#' todo()
-#'
-#' # You may track tags in you manuscript, to link text and code.
-tag <- function(msg = "") {
-  function() {
-    message(msg)
-    invisible(msg)
-  }
+tag <- function(n = 3) {
+  paste0(
+    purrr::rerun(2, sample(letters, 1)),
+    purrr::rerun(2, sample(0:9, 1)),
+    collapse = ""
+  )
 }
+
+
+
+
+
+
+
+
+
+
+
