db5394077212deeb2c3cf0f314196ec09f68fad5
maurolepore <maurolepore@gmail.com>
2017-12-09 15:33:32 -0500

    Remove tag()

--- tests/testthat/test-tag.R
+++
@@ -1,9 +0,0 @@
-context("tag")
-
-test_that("tag returns a custom message, or an empty message by default", {
-  default_message <- tag()
-  custom <- "This is a custom message"
-  custom_message <- tag(custom)
-  expect_message(default_message(), "")
-  expect_message(custom_message(), custom)
-})
